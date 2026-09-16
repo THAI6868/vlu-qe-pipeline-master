b4f317664cb4733cae582169fe7fed44f37993eb
Thai8386 <thai.2474802010354@vanlanguni.vn>
2026-09-16 19:26:43 +0700

    Implement login function in auth.js

---
+++ auth.js
@@ -0,0 +1,11 @@
+function login(username, password) {
+    if (!username || !password) {
+        return false;
+    }
+    if (username === 'admin' && password === '123') {
+        return true;
+    }
+    return false;
+}
+
+module.exports = { login };
